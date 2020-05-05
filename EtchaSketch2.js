7a98de21c4a25b1d75a134fa7219e73731720b86
Anderson Laventure <Laventurepsu1@gmail.com>
2020-05-05 12:34:16 -0400

    changing title

---
+++ EtchaSketch2.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container-grid");
+
+const div = document.createElement("div");
+div.className = "item";
+
+let i = div
+
+
+for (let i = 0; i <= 256; i++) {
+
+  container.appendChild(div)
+
+}
+
+console.log(i);
+
+
